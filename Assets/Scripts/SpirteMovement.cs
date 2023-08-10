a96f48c021a7d78fe8f21c6f9a6426699ee10356
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-08-10 04:03:46 -0700

    Create basic working assets to test with

--- Assets/Scripts/SpirteMovement.cs
+++ Assets/Scripts/SpirteMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementScript : MonoBehaviour
+public class SpirteMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
