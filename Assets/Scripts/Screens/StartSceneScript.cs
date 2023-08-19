964445656a87573896d7d61646f6db14b3ad09a6
Darshmallow <98087037+Darshmallow@users.noreply.github.com>
2023-08-19 01:09:45 -0400

    added start button

--- Assets/Scripts/Screens/StartSceneScript.cs
+++ Assets/Scripts/Screens/StartSceneScript.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StartScene : MonoBehaviour
+
+public class StartSceneScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
