723b3bd6f82b9d37202c735c777b3ed3df052a0e
Darshmallow <98087037+Darshmallow@users.noreply.github.com>
2023-08-22 21:47:22 -0700

    camera testing

--- Assets/Scripts/Player/CameraBehavior.cs
+++ Assets/Scripts/Player/CameraBehavior.cs
@@ -6,10 +6,32 @@ public class CameraBehavior : MonoBehaviour
 {
     [SerializeField] Transform playerOne;
     [SerializeField] Transform playerTwo;
+    [SerializeField] Camera c;
+
     [SerializeField] public Vector3 minValues, maxValues;
+    [SerializeField] float minZoom, maxZoom;
 
     // Update is called once per frame
     void Update()
+    {
+        float[] positions = findPosition();
+        float targetOneX = positions[0];
+        float targetOneY = positions[1];
+        float targetTwoX = positions[2];
+        float targetTwoY = positions[3];
+
+        float targetX = (targetOneX + targetTwoX) / 2;
+        float targetY = (targetOneY + targetTwoY) / 2;
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+
+        float xDistance = Mathf.Abs(targetOneX - targetTwoX);
+        float yDistance = Mathf.Abs(targetOneY - targetTwoY);
+        float z = zoom(xDistance, yDistance);
+
+        c.orthographicSize = z;
+    }
+
+    private float[] findPosition()
     {
         float targetOneX = playerOne.position.x + 1.5f;
         float targetTwoX = playerTwo.position.x + 1.5f;
@@ -31,7 +53,6 @@ public class CameraBehavior : MonoBehaviour
             targetTwoX = maxValues.x;
         }
 
-
         float targetOneY = playerOne.position.y + 1.5f;
         float targetTwoY = playerTwo.position.y + 1.5f;
         if (targetOneY < minValues.y)
@@ -52,9 +73,19 @@ public class CameraBehavior : MonoBehaviour
             targetTwoY = maxValues.y;
         }
 
-        float targetX = (targetOneX + targetTwoX) / 2;
-        float targetY = (targetOneY + targetTwoY) / 2;
-        transform.position = new Vector3(targetX, targetY, transform.position.z);
+        float[] positions = { targetOneX, targetOneY, targetTwoX, targetTwoY };
+        return positions;
+    }
 
+    private float zoom(float x, float y)
+    {
+        float xZoom = x / 200;
+        float yZoom = y / 200;
+
+        float z = xZoom;
+        if (9 * xZoom > 16 * yZoom)
+        {
+            float z = yZoom
+        }
     }
 }
