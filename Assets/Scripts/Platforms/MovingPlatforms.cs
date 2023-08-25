11624aaffe8e45f95cd7a115ca8e5d551bfd77c7
Darshmallow <98087037+Darshmallow@users.noreply.github.com>
2023-08-24 21:27:55 -0700

    created moving platforms

---
+++ Assets/Scripts/Platforms/MovingPlatforms.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforma : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //need to figure out how much update animation according to this
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+}
