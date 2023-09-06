c9561b2b390653acbd52825ed1ef687fdfea9c8b
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-09-05 21:17:36 -0700

    Fix tag for moving platform

--- Assets/Scripts/Platforms/MovingPlatforms.cs
+++ Assets/Scripts/Platforms/MovingPlatforms.cs
@@ -18,7 +18,7 @@ public class MovingPlatforma : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.transform.parent = transform;
         }
