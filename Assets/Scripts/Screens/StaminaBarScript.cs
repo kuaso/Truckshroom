3e61eca8c088f8e475a8dfa8794d4fd49c613e9c
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-09-12 13:19:32 -0700

    TODO stamina bar

---
+++ Assets/Scripts/Screens/StaminaBarScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+// TODO make stamina bar
+public class StaminaBarScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
