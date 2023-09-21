6a63d4d35a3c288db352f63f12b9c06cee51802e
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-09-21 12:40:17 -0700

    Fix bug where stamina bar would drain twice as fast as stamina

--- Assets/Scripts/Screens/StaminaBarScript.cs
+++ Assets/Scripts/Screens/StaminaBarScript.cs
@@ -10,18 +10,10 @@ public class StaminaBarScript : MonoBehaviour
     {
         _stamina = GetComponentInParent<Stamina>();
         _staminaBarMask = GetComponent<Image>();
-        Debug.LogError("TODO: Make stamina bar change colours depending on how much stamina is left");
     }
 
     private void FixedUpdate()
     {
-        /*
-         * Explanation of how fillAmount is derived:
-         * 1f -: 1f means total fill. Minus a value after this to get rid of used up stamina
-         * Math.Abs(_stamina.ShareStamina - 100f): Get the absolute value of stamina used
-         * / 100f: Divide by 100 to the amount used out of 1f
-         * * 2: Multiply by 2 to double the rate that the stamina bar drains by visually (since it drains from 2 sides)
-         */
-        _staminaBarMask.fillAmount = 1f - (Mathf.Abs(_stamina.SharedStamina - 100f) / 100f * 2) ;
+        _staminaBarMask.fillAmount = _stamina.SharedStamina / 100f;
     }
 }
