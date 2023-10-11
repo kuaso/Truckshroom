6a10f6b12e8f52c70e03970161b1d7958c57f773
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-10-11 13:40:29 -0700

    FINALLY FINISH STAMINA

--- Assets/Scripts/Player/Stamina.cs
+++ Assets/Scripts/Player/Stamina.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Stamina : MonoBehaviour
@@ -5,35 +6,45 @@ public class Stamina : MonoBehaviour
     private const float RateOfDecreasePercent = 0.4f;
     private const float RateOfIncreasePercent = 5f;
 
-
     private const float MaxStamina = 100f;
-    private static float SharedStamina { get; set; } = MaxStamina;
+    private bool canRegenerate = false;
+    private float SharedStamina { get; set; } = MaxStamina;
 
     // This automatically updates, whew! 
-    public static bool HasStamina => SharedStamina > 0f;
+    public bool HasStamina => SharedStamina > 0f;
     public delegate void OnStaminaChanged(float newStamina, float maxStamina = MaxStamina);
     public static event OnStaminaChanged StaminaChanged;
     public delegate void OnStaminaFullyRecharged();
     public static event OnStaminaFullyRecharged StaminaFullyRecharged;
     
-    public static void TickSharedStamina()
+    public void TickSharedStamina()
     {
         SharedStamina -= RateOfDecreasePercent;
         StaminaChanged?.Invoke(SharedStamina);
     }
 
-    public static void Regenerate() // TODO CONVERT TO COROUTINE THAT WE CAN CANCEL THORUGH #StopRegenerate
+    public void Regenerate()
     {
-        // We don't want to invoke the event if the stamina is already full
-        if (SharedStamina >= MaxStamina) return;
-        SharedStamina = Mathf.Min(SharedStamina + RateOfIncreasePercent, 100f);
-        StaminaChanged?.Invoke(SharedStamina);
-        if (SharedStamina < MaxStamina) return;
-        StaminaFullyRecharged?.Invoke();
+        canRegenerate = true;
+        StartCoroutine(RecoverStamina());
     }
-    
-    public static void StopRegenerate()
+
+    private IEnumerator RecoverStamina()
     {
-        // TODO IMPLEMENT
+        while (canRegenerate)
+        {
+            // We don't want to invoke the event if the stamina is already full
+            if (SharedStamina >= MaxStamina) yield return null;
+            SharedStamina = Mathf.Min(SharedStamina + RateOfIncreasePercent, 100f);
+            StaminaChanged?.Invoke(SharedStamina);
+            if (SharedStamina < MaxStamina) yield return null;
+            StaminaFullyRecharged?.Invoke();
+        }
+    }
+
+    public void StopRegenerate()
+    {
+        Debug.Log("hi");
+        canRegenerate = false;
     }
 }
