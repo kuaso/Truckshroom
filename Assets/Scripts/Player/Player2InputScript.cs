bf477a7c10e10c4b82709102665bd7c7ec764631
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-11-22 13:38:05 -0800

    Find out how to change animation states

--- Assets/Scripts/Player/Player2InputScript.cs
+++ Assets/Scripts/Player/Player2InputScript.cs
@@ -6,16 +6,18 @@ public class Player2InputScript : BasePlayerInputScript
     private const int PlayerNumber = 1;
     private PlayerInput _playerInput;
     private Rigidbody2D _rb;
+    private Animator _animator;
 
     private void OnEnable()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
         ColliderStates[PlayerNumber] = new Dictionary<Collider2D, bool>();
         foreach (var key in GetComponents<Collider2D>())
         {
             ColliderStates[PlayerNumber][key] = false;
         }
-        
+
         _playerInput = new PlayerInput();
         _playerInput.Enable();
         _playerInput.Player2.HorizontalMovement.performed += Move;
@@ -38,7 +40,7 @@ public class Player2InputScript : BasePlayerInputScript
         _playerInput.Disable();
     }
 
-    private void FixedUpdate() => UpdateLoop(_rb);
+    private void FixedUpdate() => UpdateLoop(_rb, _animator);
     private void OnCollisionEnter2D(Collision2D other) => CollisionEntered2D(other, PlayerNumber);
     private void OnCollisionExit2D(Collision2D other) => CollisionExited2D(other, PlayerNumber);
 }
